1fb18d925882c0470675fb20e977f42e1367966e
Brooke Mitchell <927600+brookemitchell@users.noreply.github.com>
2022-09-26 15:10:55 +1300

    setup change user

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -1,21 +1,24 @@
 defmodule Rumbl.Accounts do
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Josē", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
-  end
-
   def get_user(id) do
-    Enum.find(list_users(), &(&1.id == id))
+    Repo.get(User, id)
   end
 
-  def get_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user)
   end
 end
