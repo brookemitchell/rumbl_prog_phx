5256f4afe03b3397387f4b12688558e5021b7bc4
Brooke Mitchell <927600+brookemitchell@users.noreply.github.com>
2022-10-01 21:01:58 +1300

    signup login page

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -19,6 +19,7 @@ defmodule RumblWeb.Router do
     pipe_through :browser
 
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
     get "/", PageController, :index
   end
 
